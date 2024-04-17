44115d7737e78393fa50afb7664ae77cd18ef400
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-17 14:49:59 +0100

    Add project files.

---
+++ AppointmentManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AppointmentManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AppointmentManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
