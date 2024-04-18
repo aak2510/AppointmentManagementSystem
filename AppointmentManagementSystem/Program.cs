57a7c270bb17a670833f06e3a861ed434e74485f
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-18 16:24:43 +0100

    Update DB and added FName & LName!

--- AppointmentManagementSystem/Program.cs
+++ AppointmentManagementSystem/Program.cs
@@ -1,6 +1,7 @@
+using AppointmentManagementSystem.Areas.Data;
 using AppointmentManagementSystem.Data;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
