d31f093d4eaf087a36eefd7e45c9a7428943c306
Tom Stevens <TomStevensDev@gmail.com>
2024-04-20 10:54:06 +0100

    Hashing added to account management and registeration

--- AppointmentManagementSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AppointmentManagementSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -145,6 +145,9 @@ namespace AppointmentManagementSystem.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    //add default role
+                    await _userManager.AddToRoleAsync(user, "User");
+
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
