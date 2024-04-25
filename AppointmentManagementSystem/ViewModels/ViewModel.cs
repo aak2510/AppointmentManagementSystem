bacd4902905928e99330bd8e732b764083435ae7
aak2510 <akanda2500@gmail.com>
2024-04-25 15:27:23 +0100

    add comments to view model

--- AppointmentManagementSystem/ViewModels/ViewModel.cs
+++ AppointmentManagementSystem/ViewModels/ViewModel.cs
@@ -3,6 +3,7 @@ using AppointmentManagementSystem.Models;
 
 namespace AppointmentManagementSystem.ViewModels
 {
+    // Used as a link to pass in the users' personal details and appointments as a single object.
     public class ViewModel
     {
 
